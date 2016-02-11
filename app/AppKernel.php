cb54a99f20d3abe0a3e8ebd2a7cfae0a396bebff
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-02-11 13:29:22 +0100

    Add FOSUserBundle with configuration and custom theming

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,23 +8,27 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = [
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new AppBundle\AppBundle(),
-            new KevinVR\FootbelBackendBundle\FootbelBackendBundle(),
+          new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+          new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+          new Symfony\Bundle\TwigBundle\TwigBundle(),
+          new Symfony\Bundle\MonologBundle\MonologBundle(),
+          new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+          new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+          new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+          new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+          new AppBundle\AppBundle(),
+          new KevinVR\FootbelBackendBundle\FootbelBackendBundle(),
+          new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle(
+            );
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle(
+            );
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(
+            );
         }
 
         return $bundles;
@@ -47,6 +51,8 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(
+          $this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml'
+        );
     }
 }
