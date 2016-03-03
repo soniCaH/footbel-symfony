e86576224b8e7cbaa3b4339f5c1897c265114383
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-03-03 20:53:10 +0100

    Return serialized rankings per province/level

--- src/KevinVR/FootbelAPIBundle/Controller/RankingController.php
+++ src/KevinVR/FootbelAPIBundle/Controller/RankingController.php
@@ -16,6 +16,13 @@ class RankingController extends FOSRestController
     /**
      * Retrieve province rankings with optional period.
      *
+     * @param \KevinVR\FootbelBackendBundle\Entity\Province $province
+     * @param \KevinVR\FootbelBackendBundle\Entity\Season $season
+     * @param string $division
+     * @param int $period
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
      * @Route("/ranking/{shorthand_province}/{shorthand_season}/{division}/{period}", name="ranking_province", defaults={"period" = 0})
      * @ParamConverter("province", options={"mapping": {"shorthand_province": "shorthand"}})
      * @ParamConverter("season", options={"mapping": {"shorthand_season": "shorthand"}})
@@ -25,13 +32,17 @@ class RankingController extends FOSRestController
     {
         $em = $this->getDoctrine()->getManager();
 
+        // Retrieve the ranking based on shorthand.
         $level = $em->getRepository('FootbelBackendBundle:Level')->findOneBy(array('shorthand' => 'prov'));
-        $ranks = $this->getRankingPerLevel($level, $season, $province, $division, $period);
 
-        return $ranks;
+        $ranks = $this->_getRankingPerLevel($level, $season, $province, $division, $period);
+
+        $rankings = $this->_getDetails($ranks);
+
+        return $rankings;
     }
 
-    private function getRankingPerLevel(Level $level, Season $season, $province, $division, $period = 0)
+    private function _getRankingPerLevel(Level $level, Season $season, $province, $division, $period = 0)
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -46,5 +57,39 @@ class RankingController extends FOSRestController
         );
 
         return $rankings;
+    }
+
+    private function _getDetails($rankings)
+    {
+        $rank_output = array();
+
+        usort(
+            $rankings,
+            function ($a, $b) {
+                return strcmp($a->getPosition(), $b->getPosition());
+            }
+        );
+
+        foreach ($rankings as $ranking) {
+            $rank_output[] = array(
+                'season' => $ranking->getSeason()->getShorthand(),
+                'level' => $ranking->getLevel()->getShorthand(),
+                'province' => $ranking->getProvince()->getShorthand(),
+                'division' => $ranking->getDivision(),
+                'team' => $ranking->getTeam(),
+                'matches' => $ranking->getMatches(),
+                'wins' => $ranking->getWins(),
+                'losses' => $ranking->getLosses(),
+                'draws' => $ranking->getDraws(),
+                'goals_pro' => $ranking->getGoalsPro(),
+                'goals_against' => $ranking->getGoalsAgainst(),
+                'goals_diff' => $ranking->getGoalsDifference(),
+                'points' => $ranking->getPoints(),
+                'period' => $ranking->getPeriod(),
+                'position' => $ranking->getPosition(),
+            );
+        }
+
+        return $rank_output;
     }
 }
