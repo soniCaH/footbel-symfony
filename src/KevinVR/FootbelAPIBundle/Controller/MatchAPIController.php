43a349543576f54fa7b51c1f5672fd0b4461142e
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-03-05 13:31:57 +0100

    Empty matches API controller

---
+++ src/KevinVR/FootbelAPIBundle/Controller/MatchAPIController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace KevinVR\FootbelAPIBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use KevinVR\FootbelBackendBundle\Entity\Level;
+use KevinVR\FootbelBackendBundle\Entity\Province;
+use KevinVR\FootbelBackendBundle\Entity\Season;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+/**
+ * Class MatchAPIController
+ * @package KevinVR\FootbelAPIBundle\Controller
+ */
+class MatchAPIController extends FOSRestController
+{
+
+}
