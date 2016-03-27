4db0d4bfcc86e2d811fafc951a7d42de242a1037
Kevin Van Ransbeeck <kevin.vanransbeeck@amplexor.com>
2016-03-27 22:25:39 +0200

    Rename routings and bundles

--- src/KevinVR/FootbalistoBackendBundle/Tests/Controller/SeasonControllerTest.php
+++ src/KevinVR/FootbalistoBackendBundle/Tests/Controller/SeasonControllerTest.php
@@ -19,7 +19,7 @@ class SeasonControllerTest extends WebTestCase
 
         // Fill in the form and submit it
         $form = $crawler->selectButton('Create')->form(array(
-            'kevinvr_footbelbackendbundle_season[field_name]'  => 'Test',
+            'kevinvr_FootbalistoBackendBundle_season[field_name]'  => 'Test',
             // ... other fields to fill
         ));
 
@@ -33,7 +33,7 @@ class SeasonControllerTest extends WebTestCase
         $crawler = $client->click($crawler->selectLink('Edit')->link());
 
         $form = $crawler->selectButton('Update')->form(array(
-            'kevinvr_footbelbackendbundle_season[field_name]'  => 'Foo',
+            'kevinvr_FootbalistoBackendBundle_season[field_name]'  => 'Foo',
             // ... other fields to fill
         ));
 
