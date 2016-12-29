84641f57d8d496ab6c20935a4003cafaaac07e0b
Kevin Van Ransbeeck <kevin.van.ransbeeck@gmail.com>
2016-12-29 13:16:05 +0100

    Codestyle.

--- src/KevinVR/FootbalistoBackendBundle/DivisionsMapping.php
+++ src/KevinVR/FootbalistoBackendBundle/DivisionsMapping.php
@@ -7,7 +7,8 @@ namespace KevinVR\FootbalistoBackendBundle;
  * Class DivisionsMapping
  * @package KevinVR\FootbalistoBackendBundle
  */
-class DivisionsMapping {
+class DivisionsMapping
+{
     /**
      * Map a shorthand division code to a managed human-readable name.
      *
@@ -17,32 +18,32 @@ class DivisionsMapping {
      * @return string|null
      *   Authored mapping of the shorthand > full division description/name.
      */
-    public static function getMapping($division_code) {
+    public static function getMapping($division_code)
+    {
         static $mapping = array(
-            'BCD' => "Beker van Brabant (A ploeg)",
-            'BPB' => "Beker van Brabant (B ploeg)",
-            'N3B' => "3e Provinciale B",
-            'N4D' => "4e Provinciale D",
-            '3E' => "3e Provinciale E",
-            '4G' => "4e Provinciale G",
-            'N10J' => "U10 - Reeks J",
-            'N7AM' => "U7 - Reeks AM",
-            'N9M' => "U9 - Reeks M",
-            'N11I' => "U11 - Reeks I",
-            'N8M' => "U8 - Reeks M",
-            'N6H' => "U6 - Reeks H",
-            'N10H2' => "U10 - Reeks J",
-            'N7AM2' => "U7 - Reeks AM",
-            'N9J2' => "U9 - Reeks J",
-            'N11F2' => "U11 - Reeks F",
-            'N8L2' => "U8 - Reeks L",
+          'BCD' => "Beker van Brabant (A ploeg)",
+          'BPB' => "Beker van Brabant (B ploeg)",
+          'N3B' => "3e Provinciale B",
+          'N4D' => "4e Provinciale D",
+          '3E' => "3e Provinciale E",
+          '4G' => "4e Provinciale G",
+          'N10J' => "U10 - Reeks J",
+          'N7AM' => "U7 - Reeks AM",
+          'N9M' => "U9 - Reeks M",
+          'N11I' => "U11 - Reeks I",
+          'N8M' => "U8 - Reeks M",
+          'N6H' => "U6 - Reeks H",
+          'N10H2' => "U10 - Reeks J",
+          'N7AM2' => "U7 - Reeks AM",
+          'N9J2' => "U9 - Reeks J",
+          'N11F2' => "U11 - Reeks F",
+          'N8L2' => "U8 - Reeks L",
         );
 
         if (array_key_exists($division_code, $mapping)) {
             return $mapping[$division_code];
-        }
-        else {
-            return NULL;
+        } else {
+            return null;
         }
     }
 }
