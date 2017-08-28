59ed1061f6b140c94214373212c7e8416eaf641c
Kevin Van Ransbeeck <climacon@gmail.com>
2017-08-28 10:31:19 +0200

    Mappings

--- src/KevinVR/FootbalistoBackendBundle/DivisionsMapping.php
+++ src/KevinVR/FootbalistoBackendBundle/DivisionsMapping.php
@@ -25,6 +25,8 @@ class DivisionsMapping
           'BPB' => "Beker van Brabant (B ploeg)",
           'N3B' => "3e Provinciale B",
           'N4D' => "4e Provinciale D",
+          '4D' => "4e Provinciale D",
+          '3C' => "3e Provinciale C",
           '3E' => "3e Provinciale E",
           '4G' => "4e Provinciale G",
           'N10J' => "U10 - Reeks J",
@@ -38,6 +40,13 @@ class DivisionsMapping
           'N9J2' => "U9 - Reeks J",
           'N11F2' => "U11 - Reeks F",
           'N8L2' => "U8 - Reeks L",
+          '8K1' => "U8 - Reeks K",
+          '12H1' => "U12 - Reeks H",
+          '11L1' => "U11 - Reeks L",
+          '10P1' => "U10 - Reeks P",
+          '7X1' => "U7 - Reeks X",
+          '9S1' => "U9 - Reeks S",
+          '9N1' => "U9 - Reeks N",
         );
 
         if (array_key_exists($division_code, $mapping)) {
