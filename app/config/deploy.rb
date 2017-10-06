7a11495101c4e4d77578afcf288a762d8a52c0a9
Kevin Van Ransbeeck <climacon@gmail.com>
2017-10-06 10:05:31 +0200

    Test github.

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -28,6 +28,12 @@ set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
 set :use_composer, true
 set :update_vendors, true
 
+set :webserver_user, "sonicah"
+set :permission_method, :acl
+set :use_set_permissions, true
+set :file_permissions_users, ['sonicah']
+set :file_permissions_paths, [fetch(:log_path), fetch(:cache_path), "app/var", "app/cache", "web/uploads"]
+
 # Be more verbose by uncommenting the following line
 # logger.level = Logger::MAX_LEVEL
 
